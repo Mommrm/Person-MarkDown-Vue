937f9f9f17a29983ab6cffff7c0cecb1f311f844
Mommrm <1295821492@qq.com>
2023-12-08 17:04:51 +0800

    first commit

--- src/views/EditorPage.vue
+++ src/views/EditorPage.vue
@@ -1,28 +1,39 @@
 <template>
     <div class="editorpage-container">
         <div class="container-menu-bar">
-            <div class="menu-item">
+            <div class="bar-item">
                 标题
             </div>
-            <div class="menu-item">
+            <div class="bar-item">
                 斜体
             </div>
-            <div class="menu-item">
+            <div class="bar-item">
                 粗体
             </div>
-            <div class="menu-item">
+            <div class="bar-item">
                 引用
             </div>
         </div>
         <div class="container-main">
             <div class="input-area">
                 <div class="textarea-container">
-                    <textarea class="input-textarea" v-model="inputContent" placeholder="支持简单的markdown语法"></textarea>
+                    <textarea class="input-textarea" v-model="inputContent" placeholder="支持简单的markdown语法"
+                        @input="inputText()" @keydown.enter="checkEnter"></textarea>
                 </div>
             </div>
             <div class="show-area">
                 <div class="body-preview">
                     {{ inputContent }}
+                </div>
+            </div>
+        </div>
+        <div class="info-area">
+            <div class="container-info-bar">
+                <div class="bar-item">
+                    字符数: {{ textNum }}
+                </div>
+                <div class="bar-item">
+                    行数: {{ enterNum }}
                 </div>
             </div>
         </div>
@@ -34,6 +45,19 @@ export default ({
     data() {
         return {
             inputContent: "",
+            textNum: 0,
+            enterNum: 1,
+        }
+    },
+    methods: {
+        inputText() {
+            this.textNum = this.inputContent.length;
+        },
+        checkEnter(event) {
+            if (event.key === "Enter") {
+                console.log(this.enterNum);
+                this.enterNum++;
+            }
         }
     }
 })
@@ -48,18 +72,24 @@ export default ({
 
 .container-menu-bar {
     display: flex;
-    background-color: rgb(255, 255, 255);
     border-top: 1px solid rgb(212, 212, 212);
     border-bottom: 1px solid rgb(212, 212, 212);
     margin-bottom: 3px;
 }
 
-.menu-item {
+.container-info-bar {
+    display: flex;
+    border-top: 1px solid rgb(212, 212, 212);
+    border-bottom: 1px solid rgb(212, 212, 212);
+    margin-bottom: 3px;
+}
+
+.bar-item {
     padding: 2px 8px;
     cursor: pointer;
 }
 
-.menu-item:hover {
+.bar-item:hover {
     background-color: rgb(231, 231, 231);
 }
 
@@ -71,13 +101,28 @@ export default ({
 
 .input-area {
     width: 50%;
+    background-color: #eee;
 }
 
 .show-area {
     width: 50%;
     display: flex;
-    flex-wrap: wrap;
-    border-left: 1px solid grey;
+    border-left: 1px solid rgb(212, 212, 212);
+}
+
+.body-preview {
+    width: 100%;
+    height: 84vh;
+    overflow-x: hidden;
+    overflow-y: auto;
+    word-wrap: break-word;
+    word-break: break-all;
+}
+
+::-webkit-scrollbar {
+    width: 5px;
+    height: 5px;
+    background: grey;
 }
 
 .textarea-container {
@@ -90,11 +135,13 @@ export default ({
 
 .input-textarea {
     width: 90%;
-    min-height: 80vh;
+    min-height: 84vh;
     max-height: 90vh;
     border: none;
     outline: none;
+    background-color: #eee;
 }
+
 
 @media all and (max-width: 700px) {
     .show-area {
